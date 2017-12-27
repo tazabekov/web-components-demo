dbaa33c22a2596474111fd9b8b00a78cd5a47cd4
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2017-12-27 10:33:38 -0800

    Custom Element - init

--- components/binary-counter/binary-counter.js
+++ components/binary-counter/binary-counter.js
@@ -3,4 +3,11 @@ fetch("/components/binary-counter/binary-counter.html")
 .then(templateText => {
 
     const templateEl = new DOMParser().parseFromString(templateText, 'text/html').querySelector('template');
+
+    class BinaryCounter extends HTMLElement {
+        constructor(){
+            super();
+        }
+    }
+    customElements.define("binary-counter", BinaryCounter);
 });
