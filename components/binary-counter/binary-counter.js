a6ce0dd4d8190b874802f6a6a18c193256bfdaa9
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2017-12-27 10:30:55 -0800

    HTML template, fetch and parse

--- components/binary-counter/binary-counter.js
+++ components/binary-counter/binary-counter.js
@@ -1,17 +1,6 @@
 fetch("/components/binary-counter/binary-counter.html")
-.then(res => res.text())
-.then(textTemplate => {
+.then(fetchResponse => fetchResponse.text())
+.then(templateText => {
 
-    const HTMLTemplate = new DOMParser().parseFromString(textTemplate, 'text/html').querySelector('template');
-
-    class BinaryCounter extends HTMLElement {
-        constructor(){
-            super();
-            const shadowRoot = this.attachShadow({ mode: 'open' });
-            const instance = HTMLTemplate.content.cloneNode(true);
-            shadowRoot.appendChild(instance);
-        }
-
-    }
-    customElements.define("binary-counter", BinaryCounter);
+    const templateEl = new DOMParser().parseFromString(templateText, 'text/html').querySelector('template');
 });
