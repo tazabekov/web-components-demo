bb0a6864e9ca0c1f36e04b24d48ddf173d86dd7b
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2017-12-28 00:24:02 -0800

    wired up decimal and binary counter

--- components/binary-counter/binary-counter.js
+++ components/binary-counter/binary-counter.js
@@ -1,7 +1,6 @@
 const templateEl = document.createElement('template');
 templateEl.innerHTML = `
-    <span id="decimal">decimal</span>
-    <span id="binary" >binary</span>
+    <span id="decimal">decimal</span>: <span id="binary" >binary</span>
 `;
 
 export class BinaryCounter extends HTMLElement {
@@ -9,6 +8,24 @@ export class BinaryCounter extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(templateEl.content.cloneNode(true));
+    }
+
+    static get observedAttributes() { return ["count"]; }
+    attributeChangedCallback(attrName, oldVal, newVal){
+        if (newVal !== oldVal) {
+            this[attrName] = newVal;
+        }
+    }
+
+    get count() { return this._count; }
+    set count(count) {
+        if (this._count !== count) {
+            this._count = parseInt(count);
+            this.setAttribute("count", this._count);
+
+            this.shadowRoot.querySelector("#decimal").innerHTML = this._count;
+            this.shadowRoot.querySelector("#binary").innerHTML = this._count.toString(2);
+        }
     }
 }
 
