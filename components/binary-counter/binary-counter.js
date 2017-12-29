5548f0c0f45e005655c180138de576ebb26e9e2f
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2017-12-29 10:58:47 -0800

    upd

--- components/binary-counter/binary-counter.js
+++ components/binary-counter/binary-counter.js
@@ -1,6 +1,4 @@
 const templateEl = document.createElement('template');
 templateEl.innerHTML = `
-<div class="binary-counter">
     <span id="decimal">decimal</span>: <span id="binary">binary</span>
-</div>
 `;
